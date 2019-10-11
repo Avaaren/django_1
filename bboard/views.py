74765f48266bccf927f9f4b8cc4e23520889a28a
Avaaren <avaaren@mail.ru>
2019-10-11 11:06:26 +0300

    changed code

--- bboard/views.py
+++ bboard/views.py
@@ -4,8 +4,6 @@ from django.template import loader
 from .models import Bb
 
 def index(request):
-	template = loader.get_template('bboard/index.html')
-	bbs = Bb.objects.order_by('-published')
-	context = {'bbs': bbs}
-	return HttpResponse(template.render(context, request))
+	bbs = Bb.object.order_by('-published')
+	return render(request, 'bboard/	index.html', {'bbs': bbs})
 # Create your views here.
